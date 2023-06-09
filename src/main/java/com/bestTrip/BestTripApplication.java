31548343b0a796b1d9a4bccd780a7d8647bde11e
Malorf <cedric.rahbari@gmail.com>
2023-06-09 10:12:58 +0200

    first commit

---
+++ src/main/java/com/bestTrip/BestTripApplication.java
@@ -0,0 +1,13 @@
+package com.bestTrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestTripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestTripApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bestTrip/BestTripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bestTrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestTripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
